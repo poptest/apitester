5da92ee6141a5efe39e250c99b72651b6a9b8504
407081518 <mehuandiguo@126.com>
2019-04-09 17:36:59 +0800

    Add utils to find if user exist

--- utils/test/UserUtilsTester.py
+++ utils/test/UserUtilsTester.py
@@ -9,6 +9,20 @@ class MyUserUtilsTester(unittest.TestCase):
         users_list = UserUtils.get_all_users_list()
         print(users_list)
 
+    def test_if_user_exist_by_username_false(self):
+        exp_value = False
+        username = "wejoweow"
+        act_value = UserUtils.if_user_exist_by_username(username)
+
+        self.assertEqual(exp_value, act_value)
+
+    def test_if_user_exist_by_username_true(self):
+        exp_value = True
+        username = "admin"
+        act_value = UserUtils.if_user_exist_by_username(username)
+
+        self.assertEqual(exp_value, act_value)
+
 
 
 if __name__ == '__main__':
