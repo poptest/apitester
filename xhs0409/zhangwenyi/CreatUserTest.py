1fd76bc3d58b2970795b316ae33cd00bb9b0f96d
zwy123456 <1026721319@qq.com>
2019-04-10 09:39:17 +0800

    Add

--- xhs0409/zhangwenyi/CreatUserTest.py
+++ xhs0409/zhangwenyi/CreatUserTest.py
@@ -2,28 +2,20 @@ import unittest
 import requests
 from config import HOST
 class MyTestCase(unittest.TestCase):
-    def test_CreatUser(self):
-        user_url = HOST+'/user'
-        user_data={
-            "username":"zhangguoliang",
-            "password":"123456",
-            "tel":"13946342567",
-            "email":"126366@qq.com"
-        }
-        resp=requests.post(user_url,data=user_data)
-        print(resp.status_code)
-        print(resp.content)
     def test_CU1(self):
         user_url = HOST + '/user'
         user_data = {
-            "username": "pop3",
+            "username": "pop68",
             "tel": "19907131352",
             "password": "Aa12345678",
             "email": "qsong.vip@qq.com"
         }
         resp = requests.post(user_url, data=user_data)
-        print(resp.status_code)
+        Except = 200
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册失败')
         print(resp.content)
+
     def test_CU2(self):
         user_url = HOST + '/user'
         user_data = {
@@ -33,20 +25,27 @@ class MyTestCase(unittest.TestCase):
             "email": "1038764578"
         }
         resp = requests.post(user_url, data=user_data)
-        print(resp.status_code)
+        Except = 300
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册不成功')
         print(resp.content)
-        # bug1邮箱格式不正确，也能成功注册一个用户
+        # print(type(Actual))
+        # print(type(Except))
+
     def test_CU3(self):
         user_url = HOST + '/user'
         user_data = {
-            "username": "pop3",
+            "username": "pop5",
             "tel": "19907131352",
             "password": "Aa12345678",
             "email": ""
         }
         resp = requests.post(user_url, data=user_data)
-        print(resp.status_code)
+        Except = 200
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册不成功')
         print(resp.content)
+
     def test_CU4(self):
         user_url = HOST + '/user'
         user_data = {
@@ -56,8 +55,12 @@ class MyTestCase(unittest.TestCase):
             "email": "qsong.vip@qq.com"
         }
         resp = requests.post(user_url, data=user_data)
-        print(resp.status_code)
+        resp = requests.post(user_url, data=user_data)
+        Except = 400
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册不成功')
         print(resp.content)
+
     def test_CU5(self):
         user_url = HOST + '/user'
         user_data = {
@@ -67,9 +70,68 @@ class MyTestCase(unittest.TestCase):
             "email": "123@qq.com"
         }
         resp = requests.post(user_url, data=user_data)
-        print(resp.status_code)
+        Except = 300
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册不成功')
         print(resp.content)
 
+    def test_CU6(self):
+        user_url = HOST + '/user'
+        user_data = {
+            "username": "123",
+            "tel": "15237930295",
+            "password": "12345",
+            "email": "2390084498@qq.com"
+        }
+        resp = requests.post(user_url, data=user_data)
+        Except = 300
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册不成功')
+        print(resp.content)
+
+    def test_CU7(self):
+        user_url = HOST + '/user'
+        user_data = {
+            "username": "TY",
+            "tel": "15738460225",
+            "password": "Meng&cheng123",
+            "email": "157@qq.com"
+        }
+        resp = requests.post(user_url, data=user_data)
+        Except = 200
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册不成功')
+        print(resp.content)
+
+    def test_CU8(self):
+        user_url = HOST + '/user'
+        user_data = {
+            "username": "test005",
+            "tel": "",
+            "password": "mimabu123",
+            "email": ""
+        }
+        resp = requests.post(user_url, data=user_data)
+        Except = 200
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册不成功')
+        print(resp.content)
+    def test_CU9(self):
+        user_url = HOST + '/user'
+        user_data = {
+            "username": "Qaq",
+            "tel": "19907131352",
+            "password": "Aa12345678",
+            "email": "qsong.vip@qq"
+        }
+        resp = requests.post(user_url, data=user_data)
+        Except = 200
+        Actual = resp.status_code
+        self.assertEqual(Except, Actual, msg='注册不成功')
+        print(resp.content)
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
