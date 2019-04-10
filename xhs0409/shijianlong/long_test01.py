0146c8d4334c5a5237f87db7bdcd9f464bcee6d7
shijianlong <614661222@qq.com>
2019-04-10 09:14:32 +0800

    this is project

--- xhs0409/shijianlong/long_test01.py
+++ xhs0409/shijianlong/long_test01.py
@@ -1,66 +1,121 @@
 import unittest
 import requests
 from config import HOST
+from utils import UserUtils
 
 
-class MyTestCase(unittest.TestCase):
-    def test_createuser_01(self):
-        self.user_url = HOST + '/user'
+class UserTestCase(unittest.TestCase):
+        # 正常创建
+    def test_createUser_01(self):
+        user_url = HOST + '/user'
         # 定义一个字典
         user_date = {
-            "username": "pop11",
+            "username": "pop120",
             "tel": "15523456789",
             "email": "1234560@pp.com",
             "password": "123456"
         }
-        resp = requests.post(self.user_url,data=user_date)
+        resp = requests.post(user_url,data=user_date)
         print(resp.status_code)
+        #预期值
         expected = 200
-        # 获取返回的状态码
+        # 实际获取返回的状态码
         actual=resp.status_code
+        #断言
         self.assertEqual(expected,actual,msg='注册失败！')
+        # UserUtils.if_user_exist_by_username('pop120')
         # 打印内容
-        print(resp.content.decode('utf8'))
+        # print(resp.content.decode('utf8'))
 
-    def test_createuser_02(self):
         # 邮箱格式不正确
+    def test_createUser_02(self):
+        user_url = HOST + '/user'
         user_date = {
-            "username": "pop22",
+            "username": "pop221",
             "tel": "15523456789",
             "email": "1234560",
             "password": "123456"
         }
-        resp = requests.post(self.user_url,data=user_date)
+        resp = requests.post(user_url,data=user_date)
         print(resp.status_code)
-        expected = 200
+        expected = 400
         actual = resp.status_code
-        self.assertEqual(expected,actual,msg='注册失败，邮箱格式不正确！')
+        self.assertEqual(expected,actual,msg='出现BUG，邮箱格式不正确！')
 
-    def test_createuser_03(self):
+        # 邮箱输入为空
+    def test_createUser_03(self):
+        user_url = HOST + '/user'
         user_data = {
-            "username": "pop33",
+            "username": "pop331",
             "tel": "15511110000",
             "email": "",
             "password": "123456"
         }
-        resp = requests.post(self.user_url,data=user_data)
+        resp = requests.post(user_url,data=user_data)
         print(resp.status_code)
-        expected = 200
+        expected = 400
         actual = resp.status_code
-        self.assertEqual(expected,actual,msg='注册失败，邮箱不能为空！')
+        self.assertEqual(expected,actual,msg='出现BUG，邮箱不能为空！')
 
-        def test_createuser_04(self):
-            user_data = {
-                "username": "pop33",
-                "tel": "15511110000",
-                "email": "",
-                "password": "123456"
-            }
-            resp = requests.post(self.user_url, data=user_data)
-            print(resp.status_code)
-            expected = 200
-            actual = resp.status_code
-            self.assertEqual(expected, actual, msg='注册失败，邮箱不能为空！')
+        # 手机号为7位
+    def test_createUser_04(self):
+        user_url = HOST + '/user'
+        user_data = {
+            "username": "pop332",
+            "tel": "1551111",
+            "email": "",
+            "password": "123456"
+        }
+        resp = requests.post(user_url, data=user_data)
+        print(resp.status_code)
+        expected = 400
+        actual = resp.status_code
+        self.assertEqual(expected, actual, msg='出现BUG，手机号长度为11位！')
+
+        #password含有特殊字符
+    def test_createUser_05(self):
+        user_url = HOST + '/user'
+        user_data = {
+            "username": "pop333",
+            "tel": "15511110000",
+            "email": "123e@qq.com",
+            "password": "123、456"
+        }
+        resp = requests.post(user_url, data=user_data)
+        print(resp.status_code)
+        expected = 400
+        actual = resp.status_code
+        self.assertEqual(expected, actual, msg='出现BUG，密码不能含有特殊字符！')
+
+        # 手机号为空
+    def test_createUser_06(self):
+        user_url = HOST + '/user'
+        user_data = {
+            "username": "pop334",
+            "tel": "",
+            "email": "123e@qq.com",
+            "password": "123、456"
+        }
+        resp = requests.post(user_url, data=user_data)
+        print(resp.status_code)
+        expected = 400
+        actual = resp.status_code
+        self.assertEqual(expected, actual, msg='出现BUG，手机号不能为空！')
+
+        # 密码为空
+    def test_createUser_07(self):
+        user_url = HOST + '/user'
+        user_data = {
+            "username": "POP123",
+            "tel": "14711110000",
+            "email": "12113e@qq.com",
+            "password": ""
+        }
+        resp = requests.post(user_url, data=user_data)
+        print(resp.status_code)
+        expected = 400
+        actual = resp.status_code
+        self.assertEqual(expected, actual, msg='出现BUG，密码不能为空！')
 
 
 if __name__ == '__main__':
