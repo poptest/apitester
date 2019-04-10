b0c08444063ed92ed57d9ae70df9e35f558e4a11
407081518 <mehuandiguo@126.com>
2019-04-10 17:40:46 +0800

    add create user testcases

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 # Domain of test environment
-HOST = "http://0.0.0.0:4000"
+HOST = "http://0.0.0.0:7000"
+
+#User url
+CREATE_USER_URL = HOST + "/user"
--- utils/UserUtils.py
+++ utils/UserUtils.py
@@ -1,21 +1,42 @@
 import requests
 import json
 
-from config import HOST
+from config import CREATE_USER_URL
 
 
-def get_all_users_list():
-    url = HOST + '/user'
-    resp = requests.get(url)
-    users_list_str = resp.content.decode('utf-8')
-    users_dic = json.loads(users_list_str)
-    users_list = users_dic['users']
-    return users_list
+class UserAPI(object):
+
+    STATUS_CODE = 'status_code'
+    CONTENT = 'content'
+
+    @classmethod
+    def add_user(cls, username, password, email, tel):
+        __user_data = {
+            'username': username,
+            'password': password,
+            'email': email,
+            'tel': tel
+        }
+        __resp = requests.post(CREATE_USER_URL, data=__user_data)
+        __ret_val = {
+            'status_code': str(__resp.status_code),
+            'content': __resp.content
+        }
+        return __ret_val
 
 
-def if_user_exist_by_username(username):
-    users_list = get_all_users_list()
-    for user in users_list:
-        if user['username'] == username:
-            return True
-    return False
+    @classmethod
+    def get_all_users_list(cls):
+        resp = requests.get(CREATE_USER_URL)
+        users_list_str = resp.content.decode('utf-8')
+        users_dic = json.loads(users_list_str)
+        users_list = users_dic['users']
+        return users_list
+
+    @classmethod
+    def if_user_exist_by_username(cls, username):
+        users_list = cls.get_all_users_list()
+        for user in users_list:
+            if user['username'] == username:
+                return True
+        return False
